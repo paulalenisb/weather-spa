ae2c58c367b224a392ed13a51423b8fcd2e40287
Paula Lenis <paulalenisb@gmail.com>
2021-02-05 09:01:12 -0500

    Style: Adding bootstrap

--- SPA/src/App.js
+++ SPA/src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Weather from './components/views/Weather'
-import ChartWeather from './components/views/ChartWeather'
+import TodayWeather from './components/views/TodayWeather'
+import NextDaysWeather from './components/views/NextDaysWeather'
 import NotFound from './components/views/NotFound'
-import './App.css';
+// import './App.css';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path='/' component={Weather} />
-        <Route path='/NextDays' component={ChartWeather} />
+        <Route path='/Today' component={TodayWeather} />
+        <Route path='/NextDays' component={NextDaysWeather} />
         <Route component={NotFound} />
       </Switch>
     </BrowserRouter>
